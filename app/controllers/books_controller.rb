36b730cf814fe59efc563891d508bf09c8efa853
Stefano Franzin <stefano.franzin@seesaw.it>
2014-02-04 09:02:16 +0100

    Devise user auth for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /books
   # GET /books.json
