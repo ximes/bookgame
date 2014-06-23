a5037eded64ce976972cdbb08529dfa13a48a0fe
Stefano Franzin <stefano.franzin@seesaw.it>
2014-06-23 12:43:19 +0100

    Style

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,7 +11,7 @@ class Book < ActiveRecord::Base
 
 	accepts_nested_attributes_for :chapters
 
-	has_attached_file :cover, :styles => { :hi_res => "1200x1200", :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :cover, :styles => { :hi_res => "1200x1200", :large => "600x600>", :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 
   	scope :publishable, -> { where(active: true, publishable: true) }
