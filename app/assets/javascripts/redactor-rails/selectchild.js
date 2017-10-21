84d0e56417a079c5935e40bc2550c5b4f7de3674
Stefano Franzin <stefano.franzin@gmail.com>
2017-10-21 14:49:13 +0200

    Fixes and upgrades to working version

--- app/assets/javascripts/redactor-rails/selectchild.js
+++ app/assets/javascripts/redactor-rails/selectchild.js
@@ -1,28 +1,51 @@
-var RLANG = {};
+(function($)
+{
+    RedactorPlugins.selectChild = function()
+    {
+        return {
+            init: function()
+            {
+                var that = this;
 
-if (typeof RedactorPlugins === 'undefined') {
-    var RedactorPlugins = {};
-}
+                var button = this.button.add('selectChild', 'Select Child');
+                this.button.addCallback(button, that.selectChild.show);
+            },
+            getTemplate: function(){
+                content = "<select id='children-chapters'>"; 
 
-RedactorPlugins.selectChild = {
-    init: function() {
-        this.buttonAdd('selectChild', "Select Child", function(){
-        	this.createModal()
-        });
+                $.each($('#chapter-list select'), function(i,val) {
+                    content = content + $(val).html();
+                });
+                console.log(content);
+                return String() + content + "</select>";
+            },
+            show: function(){
+                this.modal.addTemplate('selectChild', this.selectChild.getTemplate());
+                this.modal.load('selectChild', 'Select Child', 600);
+                
+                this.modal.show();
+ 
+                $('select#children-chapters').on('change', this.selectChild.insert);
+                $('select#children-chapters').focus();
+            },
+            load: function(i,s)
+            {
+                $(s).on('click', $.proxy(function(e)
+                {
+                    e.preventDefault();
+                    this.clips.insert($(s).next().html());
 
-        //Add a separator before the button
-        this.buttonAddSeparatorAfter('selectChild');
-    },
-    createModal: function() {
-        "use strict";
-
-        var that = this;
-        this.modalInit(RLANG.emoticons, "#chapter-list", 300, function() {
-            $('select#child_chapter').change(function() {
-                that.bufferSet();
-                that.insertHtml('<p>Go to chapter [link_to_chapter ' + $(this).val() + ']</p>');
-                that.modalClose();
-            });
-        });
-    }
-};
+                }, this));
+            },
+            insert: function(html)
+            {
+                var html = '<p>Go to chapter [link_to_chapter ' + $('select#children-chapters').val() + ']</p>'
+                this.modal.close();
+ 
+                this.buffer.set(); // for undo action
+                this.insert.html(html);
+                return true;
+            }
+        };
+    };
+})(jQuery);
